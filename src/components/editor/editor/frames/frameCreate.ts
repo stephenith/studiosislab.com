5aac1070a933ffb440f61ee9584671d40b7675b0
Stephen Pereira <pstephen027@gmail.com>
2026-04-24 09:33:56 +0530

    Fix Fabric frame group type error

--- src/components/editor/editor/frames/frameCreate.ts
+++ src/components/editor/editor/frames/frameCreate.ts
@@ -116,7 +116,6 @@ export function createFrameCreate(deps: {
       hasBorders: true,
       hasControls: true,
       perPixelTargetFind: true,
-      layout: 'clip-path',
       objectCaching: true,
       subTargetCheck: true,
       interactive: true,
